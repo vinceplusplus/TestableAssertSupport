89ee981347f3c75511011a1657b2043e6ac0eb62
Vincent Cheung <vinceplusplus@gmail.com>
2019-10-20 10:00:47 +0800

    Initial commit

---
+++ Sources/TestableAssertSupport/AssertFuncBodies.swift
@@ -0,0 +1,8 @@
+public struct AssertFuncBodies {
+  public static var assert = Swift.assert
+  public static var assertionFailure = Swift.assertionFailure
+  public static var precondition = Swift.precondition
+  public static var preconditionFailure = Swift.preconditionFailure
+  public static var fatalError = Swift.fatalError
+}
+
